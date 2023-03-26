cb92420c273826684c143265f39329dfdbac4f49
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-03-26 10:45:47 +0500

    Add project files.

---
+++ 10.0/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double number = 0;
+        bool validNumber = false;
+
+        while (!validNumber)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+
+            if (double.TryParse(input, out number))
+            {
+                validNumber = true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, please try again.");
+            }
+        }
+
+        int firstNumber = 0, secondNumber = 1;
+
+        Console.Write(firstNumber + " " + secondNumber + " ");
+
+        for (int i = 2; i < number; i++)
+        {
+            int thirdNumber = firstNumber + secondNumber;
+            Console.Write(thirdNumber + " ");
+            firstNumber = secondNumber;
+            secondNumber = thirdNumber;
+        }
+
+        Console.WriteLine();
+    }
+}
